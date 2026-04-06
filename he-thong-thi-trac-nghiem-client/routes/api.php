60553a3284c632b6b7f971d7ecaba1e6b8af6c5b
soranamida1 <lp.a7.0404@gmail.com>
2026-04-07 00:02:07 +0700

    merge

--- he-thong-thi-trac-nghiem-client/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController; // Đảm bảo bạn đã có UserController ở project Service này nhé
-
-
-Route::get('/users', [UserController::class, 'index']);
--- he-thong-thi-trac-nghiem-client/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
-Route::get('/', [UserController::class, 'index']);
-Route::post('/add-user', [UserController::class, 'store']);
